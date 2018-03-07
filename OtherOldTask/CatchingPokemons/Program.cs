4f077a2338204d7d8a000fc706a8eacf8affc428
iTsvetelin <c.istatkov@abv.bg>
2018-03-07 10:28:30 +0200

    Let see

---
+++ OtherOldTask/CatchingPokemons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CatchingPokemons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
